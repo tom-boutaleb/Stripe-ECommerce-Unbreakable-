5fcfc8a43a4d7eafb87bca331559ee86bcb774d6
Theo <theo.petropoulos@laplateforme.io>
2021-12-28 16:08:58 +0100

    typo fix

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -104,7 +104,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      *
      * @see UserInterface
      */
-    public function getUserentifier(): string
+    public function getUserIdentifier(): string
     {
         return (string) $this->email;
     }
